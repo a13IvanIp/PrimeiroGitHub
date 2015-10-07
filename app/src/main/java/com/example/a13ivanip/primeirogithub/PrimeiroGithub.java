03a9762da0a4025849f10c4be4b5bb2249f5dfac
dam2 - Insua Pazos <a13ivanip@a11equ30.sanclemente.local>
2015-10-07 12:46:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a13ivanip/primeirogithub/PrimeiroGithub.java
@@ -0,0 +1,13 @@
+package com.example.a13ivanip.primeirogithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrimeiroGithub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeiro_github);
+    }
+}
